ae061c35545e00db60e960c02fcd4bbb7d6f4808
Vison <kk801030@gmail.com>
2014-12-05 14:07:02 +0800

    wisher create, read testing

---
+++ routes/wisher.js
@@ -0,0 +1,30 @@
+var express  = require('express');
+var router   = express.Router();
+
+var mongoose = require('mongoose');
+var Wisher   = mongoose.model('Wisher');
+
+
+router.post('/create', function createWisher(req, res){
+  new Wisher({
+    name         : 'myname',
+    school_id    : 'myid'
+  }).save(function(err, Wisher, count){
+
+  });
+});
+
+router.get('/', function index(req, res) {
+  Wisher.
+  find().
+  exec(function (err, todos, count) {
+    res.render('index', {
+      title : 'Express Todo Example',
+        todos : todos
+      });
+  });
+});
+
+
+
+module.exports = router;
