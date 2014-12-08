0a368c1b809e2d4b44f5e31cfbb53fd5cf29b417
Vison <kk801030@gmail.com>
2014-12-08 19:56:27 +0800

    merge

--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,10 @@
 var express  = require('express');
 var router   = express.Router();
-
 var mongoose = require('mongoose');
 var Wisher   = mongoose.model('Wisher');
 
 router.get('/', function index(req, res) {
-  res.render('admin', {
-    title : 'Admin Page',
-  });
+  res.render('admin', { title : 'Admin Page' });
 });
 
 module.exports = router;
