8e6e9362e03c18bbe65ad8040c2ef7be8ff47c73
Vison <kk801030@gmail.com>
2014-12-16 03:36:46 +0800

    ntusa logo

--- public/js/animate.js
+++ public/js/animate.js
@@ -8,14 +8,17 @@ $(function (){
   wow.init();
   //SmoothScrolling();
   setBackgroundHeight();
-
+  var ntusa_start = $('#ntusa').offset().top;
   $( window ).scroll(function() {
     var speed = 100;
     var position = $(document).scrollTop();
     var move = - position * (speed / 1000);
+    var move2 = - position * (speed*1.1 / 1000);
     //$('#log').html('move:'+move+" position:"+position);
     //$('#log').css('top', position);
     $('#bg').css('backgroundPosition', '0px ' + move + 'px');
+
+    $('#ntusa').css('top', (ntusa_start+move2) + 'px');
     /*$('#bg').animate({
       'background-position-y':  move + 'px'
     },20,'linear');
